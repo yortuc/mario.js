605ec63993bda1bdd81c2942b42b2c0f68a4bb3f
Evren Yortuçboylu <yortucboylu@gmail.com>
2018-01-27 20:57:06 +0100

    add gravity

--- public/js/index.js
+++ public/js/index.js
@@ -1,40 +1,15 @@
 import { loadImage, loadLevel } from './loader.js';
 import { loadMarioSprite, loadBackgroundSprites } from './sprites.js';
-
-function drawBackground(background, context, sprites){
-	background.ranges.forEach(([x1, x2, y1, y2])=> {
-		for(let i=x1; i<x2; ++i){
-			for(let j=y1; j<y2; ++j){
-				sprites.drawTile(background.tile, context, i, j);
-			}
-		}
-	})
-}
+import Compositor from './Compositor.js';
+import { createBackgroundLayer } from './layers.js';
 
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
+const gravity = 0.5;
 
-class Compositor {
-	constructor() {
-		this.layers = [];
-	}
-
-	draw(context){
-		this.layers.forEach(layer => layer(context));
-	}
-}
-
-function createBackgroundLayer(backgrounds, sprites){
-	const buffer = document.createElement('canvas');
-	buffer.width = canvas.width;
-	buffer.height = canvas.height;
-
-	backgrounds.forEach(background=> 
-		drawBackground(background, buffer.getContext('2d'), sprites)
-	);
-
-	return function drawBackgroundLayer(context){
-		context.drawImage(buffer, 0, 0);
+function createSpriteLayer(sprite, pos){
+	return function drawSpriteLayer(context){
+		sprite.draw('idle', context, pos.x, pos.y);
 	}
 }
 
@@ -42,18 +17,22 @@ Promise.all([
 	loadMarioSprite(),
 	loadBackgroundSprites(), 
 	loadLevel('1-1')
-]).then(([marioSprite, sprites, level])=>{
+]).then(([marioSprite, backgroundSprites, level])=>{
 	const comp = new Compositor();
-	const backgroundLayer = createBackgroundLayer(level.backgrounds, sprites);
+	const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
 	comp.layers.push(backgroundLayer);
 	
 	const pos = { x: 62, y: 64 };
+	const velocity = { x: 2, y: -10 };
+
+	const spriteLayer = createSpriteLayer(marioSprite, pos);
+	comp.layers.push(spriteLayer);
 
 	function update(){
 		comp.draw(context);
-		marioSprite.draw('idle', context, pos.x, pos.y);
-		pos.x += 2;
-		pos.y += 2;
+		pos.x += velocity.x;
+		pos.y += velocity.y;
+		velocity.y += gravity;
 		requestAnimationFrame(update);
 	}
 
---
+++ public/js/layers.js
@@ -0,0 +1,23 @@
+function drawBackground(background, context, sprites){
+	background.ranges.forEach(([x1, x2, y1, y2])=> {
+		for(let i=x1; i<x2; ++i){
+			for(let j=y1; j<y2; ++j){
+				sprites.drawTile(background.tile, context, i, j);
+			}
+		}
+	})
+}
+
+export function createBackgroundLayer(backgrounds, sprites){
+	const buffer = document.createElement('canvas');
+	buffer.width = canvas.width;
+	buffer.height = canvas.height;
+
+	backgrounds.forEach(background=> 
+		drawBackground(background, buffer.getContext('2d'), sprites)
+	);
+
+	return function drawBackgroundLayer(context){
+		context.drawImage(buffer, 0, 0);
+	}
+}
