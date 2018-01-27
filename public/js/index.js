58e57714ffe6e367b009606a4a2001adc71477b9
Evren Yortuçboylu <yortucboylu@gmail.com>
2018-01-27 19:23:05 +0100

    seperate layer drawing logic

--- public/js/index.js
+++ public/js/index.js
@@ -14,24 +14,43 @@ function drawBackground(background, context, sprites){
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
+class Compositor {
+	constructor() {
+		this.layers = [];
+	}
+
+	draw(context){
+		this.layers.forEach(layer => layer(context));
+	}
+}
+
+function createBackgroundLayer(backgrounds, sprites){
+	const buffer = document.createElement('canvas');
+	buffer.width = canvas.width;
+	buffer.height = canvas.height;
+
+	backgrounds.forEach(background=> 
+		drawBackground(background, buffer.getContext('2d'), sprites)
+	);
+
+	return function drawBackgroundLayer(context){
+		context.drawImage(buffer, 0, 0);
+	}
+}
+
 Promise.all([
 	loadMarioSprite(),
 	loadBackgroundSprites(), 
 	loadLevel('1-1')
 ]).then(([marioSprite, sprites, level])=>{
-
-	const backgroundBuffer = document.createElement('canvas');
-	backgroundBuffer.width = canvas.width;
-	backgroundBuffer.height = canvas.height;
-
-	level.backgrounds.forEach(background=> 
-		drawBackground(background, backgroundBuffer.getContext('2d'), sprites)
-	);
-
+	const comp = new Compositor();
+	const backgroundLayer = createBackgroundLayer(level.backgrounds, sprites);
+	comp.layers.push(backgroundLayer);
+	
 	const pos = { x: 62, y: 64 };
 
 	function update(){
-		context.drawImage(backgroundBuffer, 0, 0);
+		comp.draw(context);
 		marioSprite.draw('idle', context, pos.x, pos.y);
 		pos.x += 2;
 		pos.y += 2;
