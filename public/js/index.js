abab238f1d28dc1d7751a94d89e078d68e7e19cc
Evren Yortuçboylu <yortucboylu@gmail.com>
2018-01-28 10:17:59 +0100

    decouple internal rendering framerate from computers framerate

--- public/js/index.js
+++ public/js/index.js
@@ -14,26 +14,32 @@ Promise.all([
 ]).then(([mario, backgroundSprites, level])=>{
 	const comp = new Compositor();
 	const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
-	comp.layers.push(backgroundLayer);
+	// comp.layers.push(backgroundLayer);
 	
 	const spriteLayer = createSpriteLayer(mario);
 	comp.layers.push(spriteLayer);
 
-	const gravity = 50;
+	const gravity = 30;
 	mario.pos.set(62, 180);
-	mario.vel.set(200, -600);
+	mario.vel.set(200, -400);
 
 	let lastTime = 0;
-	let deltaTime = 0;
+	const deltaTime = 1/60;
+	let accumulatedTime = 0;
 
 	function update(time){
-		deltaTime = (time - lastTime)/1000;
+		accumulatedTime += (time - lastTime)/1000;
 
-		comp.draw(context);
-		mario.update(deltaTime);
-		mario.vel.y += gravity;
+		while(accumulatedTime > deltaTime){
+			comp.draw(context);
+			mario.update(deltaTime);
+
+			mario.vel.y += gravity;
+			requestAnimationFrame(update);
+
+			accumulatedTime -= deltaTime;
+		}
 		requestAnimationFrame(update);
-
 		lastTime = time;
 	}
 
